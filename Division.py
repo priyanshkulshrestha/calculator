224272c26fb59321996abe415bd20dd1c992d369
cactus_1549 <50070865+saikumar0512@users.noreply.github.com>
2019-11-15 12:13:58 +0530

    added exception

--- Division.py
+++ Division.py
@@ -3,7 +3,8 @@ def Division():
   a  = int(input("enter any number"))
 
   b = int(input("enter any number"))
-
-  c = a/b
-
+  try:
+    c = a/b
+  except:
+    print('cant divide by zero')
   return c 
