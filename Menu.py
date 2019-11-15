c2e62011fff36c817deea4b4f56c0c0887d70141
Eswarteja14 <54432232+Eswarteja14@users.noreply.github.com>
2019-11-15 13:17:09 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -2,14 +2,14 @@ choice = input("Enter 1 for addition, 2 for subtraction, 3 for multiplication, a
 a = input("enter the first number:")
 b = input("enter the second number:")
 if choice == "1":
-	import add as a1
+	import Addition as a1
 	a1.add(a, b)
 if choice == "2":
-	import sub as s1
+	import Subtraction as s1
 	s1.sub(a, b)
 if choice == "3":
-	import multiply as m1
+	import multiplication_fucntion as m1
 	m1.mul(a, b)
 if choice == "4":
-	import div as d1
+	import Division as d1
 	d1.div(a, b)
