25c4bd8cdb9137297774f9837a0796192317e379
cactus_1549 <50070865+saikumar0512@users.noreply.github.com>
2019-11-20 20:36:45 +0530

    updated

--- Menu.py
+++ Menu.py
@@ -1,4 +1,4 @@
-choice = input("Enter 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division :")
+choice = input("Enter 1 for addition, 2 for subtraction, 3 for multiplication,4 for division,and 5 for modulo :")
 a = int(input("enter the first number:"))
 b = int(input("enter the second number:"))
 if choice == "1":
